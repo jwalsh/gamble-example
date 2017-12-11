b3f581c12b1257bd1202395c8123de4108e27664
Jason Walsh <jwalsh@saylent.com>
2017-12-11 11:27:30 -0500

    Coin examples

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # gamble-example
+
+## Getting Started 
+
+```
+raco pkg install git://github.com/rmculpepper/gamble
+raco pkg install plot-gui-lib
+```
--- models.rkt
+++ models.rkt
@@ -1,5 +1,8 @@
 #lang gamble
 
+
+(require plot)
+
 (define (flip [n .5])
   (if (> (random) n)
       #t
@@ -17,3 +20,32 @@
 ;;    (define A (flip))
 ;;    (define B (flip))
 ;;    (or A B)))
+
+;; 1.1
+(define s-first-given-or
+  (rejection-sampler
+   (define A (flip))
+   (define B (flip))
+   (observe/fail (or A B))
+   A))
+
+(s-first-given-or)
+
+
+(sampler->mean+variance s-first-given-or 100 (indicator #t))
+
+(sampler->discrete-dist s-first-given-or 100)
+(discrete-dist [#f 0.32] [#t 0.68])
+
+
+;; (hist (repeat s-first-given-or 100))
+
+
+;; 1.2
+(define (count-true-flips n)
+    (if (zero? n)
+        0
+        (+ (if (flip) 1 0)
+           (count-true-flips (sub1 n)))))
+
+(count-true-flips 100)
